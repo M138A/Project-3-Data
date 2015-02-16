3f92ce81f61a4a4da2c2b3b1c46faf322d72fc1f
barthr <bartfokker@gmail.com>
2015-02-16 22:59:02 +0100

    account toevoegen bijna af

--- with DB/src/ProjectData/adminEnvironment.java
+++ with DB/src/ProjectData/adminEnvironment.java
@@ -3,12 +3,14 @@ package ProjectData;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
 
 
 public class adminEnvironment extends loginForm {
     //the authorized user, which will be set in the constructor
     authorizedUser currentUser = null;
-
+    private static String[] userGrant = {"", "1", "2"};
+    private static JComboBox<String> userList = new JComboBox<String>(userGrant);
 
 
     /**
@@ -36,29 +38,62 @@ public class adminEnvironment extends loginForm {
         x.add(createPasswordLabel(10, 50));
         x.add(createPassWordField(90, 50));
         x.add(createComboBox(90, 80));
-        x.add(createValidationButton(90,120));
+        x.add(createValidationButton(90, 120));
         x.setVisible(true);
         x.setSize(300, 300);
     }
+
     public static JComboBox<String> createComboBox(int x, int y) {
-        String[] userGrant = {"","Administrator", "Analyst"};
-        JComboBox<String> userList = new JComboBox<String>(userGrant);
-        userList.setBounds(x,y,100,20);
+        userList.setBounds(x, y, 100, 20);
         return userList;
     }
 
     public static JButton createValidationButton(int x, int y) {
         JButton btnAddAccount = new JButton("Voeg Account Toe");
-        btnAddAccount.setBounds(x,y,100,20);
+        btnAddAccount.setBounds(x, y, 100, 20);
         btnAddAccount.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String username = userField.getText();
                 String password = String.valueOf(passField.getPassword());
-
-                System.out.println(username+password);
+                String functions = (String) userList.getSelectedItem();
+                System.out.println(username + password + functions);
             }
         });
         return btnAddAccount;
     }
+
+    public boolean checkCredentials(String username1, String password1, String functions1) {
+        Connection conn = null;
+        String dbPass = null;
+        crypt encryptAES = new crypt();
+
+        try {
+            crypt.encrypt(password1);
+            Class.forName("com.mysql.jdbc.Driver");
+            //connect to database
+            conn = DriverManager.getConnection("jdbc:mysql://mijnmarklinbaan.nl/mijnma1q_PrjData", "mijnma1q_prjuser", "password");
+            Statement statement = conn.createStatement();
+            String sql;
+            sql = "INSERT INTO \"Leden\" (ID, \"username\",\"password\", \"role\") \n" +
+                    "VALUES(4 , username1,password1, 2);";
+            //execute sql query
+            ResultSet rs = statement.executeQuery(sql);
+            //process result of query
+            //close database connection
+            conn.close();
+
+        } catch (SQLException e) {//throwed when there is an sql exception
+            e.printStackTrace();
+            return false;
+        } catch (ClassNotFoundException e) {//throwed when com.mysql.jdbc.Driver is not found
+
+            e.printStackTrace();
+            return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
 }
+
