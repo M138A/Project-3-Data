e1ab514a2224e13f50d270445b3f3702f3f0338f
barthr <bartfokker@gmail.com>
2015-03-09 00:18:38 +0100

    Scene swaps

--- Java FX/src/Data_Project/Controller.java
+++ Java FX/src/Data_Project/Controller.java
@@ -2,8 +2,15 @@ package Data_Project;
 
 
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 
 public class Controller{
@@ -11,11 +18,25 @@ public class Controller{
     @FXML TextField userField;
     @FXML PasswordField passField;
     @FXML Label errorLabel;
+
+
+    public Controller() {
+        super();
+    }
+
     public void submitForm(ActionEvent actionEvent) {
        authorizedUser user = new authorizedUser();
         if(!user.checkCredentials(userField.getText(), passField.getText()))
         {
             errorLabel.setText("Invalid credentials");
         }
+    }
+
+    public Button getSubmitButton() {
+        return submitButton;
+    }
+
+    public void goToScreen2(Event event) {
+        System.out.println("test");
     }
 }
--- Java FX/src/Data_Project/Main.java
+++ Java FX/src/Data_Project/Main.java
@@ -6,22 +6,31 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.io.IOException;
 
+public class Main extends Application {
+    public Stage window;
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Log in");
-
-        primaryStage.setScene(new Scene(root, 500, 500));
-        primaryStage.show();
-
-
+        window = primaryStage;
+        //Scene1
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        FXMLLoader loader1 = new FXMLLoader(getClass().getResource("Admin.fxml"));
+        Parent root = loader.load();
+        Parent root1 = loader1.load();
+        Controller a1 = loader.getController();
+        Admin a2 = loader1.getController();
+          a1.getSubmitButton().setOnAction(e -> {
+             window.setScene(new Scene(root1,500,500));
+            });
+        window.setTitle("Log in");
+        window.setScene(new Scene(root,500,500));
+        window.show();
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
+    public static void main(String[] args) throws IOException {
+       launch(args);
 
     }
 }
