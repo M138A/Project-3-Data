d03d5ac7126b69fb7b01d010c7f4ee391adc6531
M138A <0896375@hr.nl>
2015-03-18 17:04:32 +0100

    Added text searcher

---
+++ Java FX/Java FX/src/Data_Project/text.java
@@ -0,0 +1,29 @@
+package Data_Project;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+/**
+ * Created by MarkGame on 18-3-2015.
+ */
+public class text {
+    public int checkString(String x) {
+        ArrayList<String> goodWords = new ArrayList<String>();
+        try {
+            BufferedReader bfr = new BufferedReader(new FileReader(getClass().getResource("positief.txt").getPath()));
+            while (bfr.readLine() != null) {
+                goodWords.add(bfr.readLine());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < goodWords.size(); i++) {
+            if (x.contains(goodWords.get(i))) {
+                return 1;
+            }
+        }
+        return -1;
+
+    }
+}
