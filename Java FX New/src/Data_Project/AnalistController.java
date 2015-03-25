5c94288a66e90c9ec5a893467d1f83270d601085
RHeijnen <heijnenrene@gmail.com>
2015-03-25 18:33:02 +0100

    Update data screen

--- Java FX New/src/Data_Project/AnalistController.java
+++ Java FX New/src/Data_Project/AnalistController.java
@@ -1,19 +1,11 @@
 package Data_Project;
 
-import facebook4j.*;
-import facebook4j.ResponseList;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.TextField;
-import se.walkercrou.places.GooglePlaces;
-import se.walkercrou.places.Place;
-import se.walkercrou.places.Review;
-import twitter4j.GeoLocation;
-import twitter4j.Paging;
-import twitter4j.*;
 
 import java.net.URL;
 import java.sql.Connection;
@@ -21,9 +13,6 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 import java.util.ResourceBundle;
 
 
@@ -51,8 +40,7 @@ public class AnalistController implements Initializable {
     public TextField outputTempArea;
     @FXML
     public TextField outputTempDisc;
-    @FXML
-    public TextField inputTextArea;
+
 
     @FXML // log out scherm
     private void logoutButtonAction() {
@@ -62,274 +50,16 @@ public class AnalistController implements Initializable {
 
     @FXML // data scherm,
     private void dataButtonAction() {
-        fxmlController logout = new fxmlController();
+        fxmlController Data = new fxmlController();
         DataChartController dc = new DataChartController();
-        logout.setLogin("Data", "Data.fxml");
+        Data.setLogin("Data", "Data.fxml");
+    }
+    @FXML // data scherm,
+    private void UpdatedataButtonAction() {
+        fxmlController UD = new fxmlController();
+        UD.setMainStage("Update Data", "UpdateDataWindow.fxml");
     }
 
-    @FXML // haalt tweets op van een timelijn
-    private void TwitzoekButtonAction() {
-
-            String inp = inputTextArea.getText();
-            // input > twitternaam > return timelijn/tweets naar output
-
-            try {
-                Paging page = new Paging (1, 50); // aantal tweets'perpage'
-                Twitter latestTweetChecker = new TwitterFactory().getInstance();
-                List<Status> statuses = latestTweetChecker.getUserTimeline(inp, page);
-                System.out.println("Showing " + " " + inp + " " + "timeline.\r\n \r\n");
-                String Message;
-                String Usrname;
-                int FollowerCount;
-                int RetweetCount;
-                int FavoriteCount;
-                String ID;  // SQL MSG ID
-
-                for (Status status : statuses) {
-
-                    // variable
-                    Message =  status.getText();
-                    Usrname =  status.getUser().getName();
-                    FollowerCount = status.getUser().getFollowersCount();
-                    RetweetCount = status.getRetweetCount();
-                    FavoriteCount = status.getFavoriteCount();
-                    ID = String.valueOf(status.getId());
-
-                    //sql connectie
-                    Connection con = connect.connectToDb();
-                    String sql = "INSERT INTO Bericht (BerichtID,Datum, Beschrijving,socialmedia,Positief) VALUES (?,?,?,?,?)";
-                    PreparedStatement preparedStatement = con.prepareStatement(sql);
-                    preparedStatement.setString(1, ID);
-                    preparedStatement.setDate(2, Date.valueOf(LocalDate.now()));
-                    preparedStatement.setString(3, Message);
-                    preparedStatement.setString(4, "Twitter");
-                    preparedStatement.setInt(5, positief.getRating(Message));
-                    preparedStatement.execute();
-
-                    sql = "INSERT INTO twitter (Bericht_BerichtID,retweet, favorite,username,gerelateerd,volgercount) VALUES (?,?,?,?,?,?)";
-                    PreparedStatement preparedStatement2 = con.prepareStatement(sql);
-                    preparedStatement2.setString(1, ID);
-                    preparedStatement2.setInt(2, RetweetCount);
-                    preparedStatement2.setInt(3, FavoriteCount);
-                    preparedStatement2.setString(4, Usrname);
-                    preparedStatement2.setInt(5, 1);
-                    preparedStatement2.setInt(6, FollowerCount);
-                    preparedStatement2.execute();
-                    /**Close connection with Database **/
-                     con.close();
-                //textareaoutput
-                    System.out.println(status.getUser().getName() + ":" +
-                            status.getText() + "\r\n");
-                }
-            }catch (TwitterException te) {
-                te.printStackTrace();
-                System.out.println("Failed : " + te.getMessage());
-                System.exit(0);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-    }
-
-    @FXML// haalt tweets op van een hashtagg
-    private void HashtaggButtonAction() { //twitter api non oob
-        String input = inputTextArea.getText(); // kijkt wat je hebt getypt
-        int socialmediaID = 0;
-        try {
-
-            twitter4j.Twitter twitter =  TwitterFactory.getSingleton();
-            Query query = new Query(input);
-            query.setCount(50);// aantal tweets LAG!
-            QueryResult result = twitter.search(query);
-            String ID;              // SQL MSG ID
-            for (Status status : result.getTweets()) {// print uit
-
-                String Usrname = status.getUser().getScreenName();
-                int FollowerCount = status.getUser().getFollowersCount();
-                int RetweetCount = status.getRetweetCount();
-                String Message = status.getText();
-                GeoLocation Location = status.getGeoLocation();
-                int FavoriteCount = status.getFavoriteCount();
-                ID = String.valueOf(status.getId());      //SQL MSG ID
-                String end = "=-=-=-=-=-=";
-
-
-                System.out.println("Naam : @" + Usrname + " : " +
-                        "\n\r volger count : " + FollowerCount +
-                        "\n\r bericht : " + Message +
-                        "\n\r locatie :" + Location +
-                        "\n\r fav count:" + FavoriteCount +
-                        "\n\r retweet count :" + RetweetCount +
-                        "\n\r " + end +
-                        "\n\r");
-                try {
-                    Connection con = connect.connectToDb();
-                    String sql = "INSERT INTO Bericht (BerichtID,Datum, Beschrijving,socialmedia,Positief) VALUES (?,?,?,?,?)";
-                    PreparedStatement preparedStatement = con.prepareStatement(sql);
-                    preparedStatement.setString(1, ID);
-                    preparedStatement.setDate(2, Date.valueOf(LocalDate.now()));
-                    preparedStatement.setString(3, Message);
-                    preparedStatement.setString(4, "Twitter");
-                    preparedStatement.setInt(5,  positief.getRating(Message));
-                    preparedStatement.execute();
-
-                    sql = "INSERT INTO twitter (Bericht_BerichtID,retweet, favorite,username,gerelateerd,volgercount) VALUES (?,?,?,?,?,?)";
-                    PreparedStatement preparedStatement2 = con.prepareStatement(sql);
-                    preparedStatement2.setString(1, ID);
-                    preparedStatement2.setInt(2, RetweetCount);
-                    preparedStatement2.setInt(3, FavoriteCount);
-                    preparedStatement2.setString(4, Usrname);
-                    preparedStatement2.setInt(5, 1);
-                    preparedStatement2.setInt(6, FollowerCount);
-                    preparedStatement2.execute();
-                    /**Close connection with Database **/
-                    con.close();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }catch (TwitterException te) { // error message
-            te.printStackTrace();
-            System.out.println("Failed : " + te.getMessage());
-            System.exit(0);
-        }
-    }
-
-    @FXML// haalt rating van google places op
-    private void PlacesButtonAction() {
-
-        GooglePlaces client = new GooglePlaces("AIzaSyALbXTMU7FfHrYpokHmOYpvJBsXUioQYlg");
-        ArrayList<Place> places = (ArrayList<Place>) client.getPlacesByQuery("RdamCentraal", GooglePlaces.MAXIMUM_RESULTS);
-        int review = 0;
-        List<Review> l1 = null;
-        Random random = new Random(); // random ID GEN
-        String ID; // SQL MSG ID
-        for (int i = 0; i < places.size(); i++) {
-            // variables
-            ID = "GP"+ String.valueOf(random.nextInt(55123124) + 15123); // SQL MSG ID
-            Place me = places.get(i);
-            List<Review> da = places.get(i).getReviews();
-            String test = places.get(i).getPlaceId();
-            Double rating = me.getRating();
-            // if geen rating, do nothing
-            System.out.println(test);
-            if (rating.equals("-1.0")) {
-                continue;
-            }else {
-                try {
-                    Connection con = connect.connectToDb();
-
-/*                    String sql = "INSERT INTO Bericht (BerichtID,Datum, Beschrijving,socialmedia,Positief) VALUES (?,?,?,?,?)";
-                    PreparedStatement preparedStatement = con.prepareStatement(sql);
-                    preparedStatement.setString(1, ID);
-                    preparedStatement.setDate(2, Date.valueOf(LocalDate.now()));
-                    preparedStatement.setString(3, "Google Location Rating");
-                    preparedStatement.setString(4, "Google");
-                    preparedStatement.setInt(5, 0);
-                    preparedStatement.execute();
-
-                    sql = "INSERT INTO google (Bericht_BerichtID,rating) VALUES (?,?)";
-                    PreparedStatement preparedStatement2 = con.prepareStatement(sql);
-                    preparedStatement2.setString(1, ID);
-                    preparedStatement2.setDouble(2, rating);
-                    preparedStatement2.execute();
-                    System.out.println("Google+ Rating updated");*/
-
-
-                    /**Close connection with Database **/
-                    con.close();
-                    /**Catch exception when data can't be saved into database for example: There is nothing filled in **/
-                } catch (SQLException e) {
-                    System.out.println("geen nieuwe updates");
-                    e.printStackTrace();
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                    System.out.println("Class niet gevonden");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-            }
-
-        }
-    }
-    /*
-     https://developers.facebook.com/tools/explorer/145634995501895/?method=GET&path=me%3Ffields%3Did%2Cname&version=v2.2
-      */
-
-    @FXML // haalt facebook posts op van de facebook pagina Rotterdam centraal
-    private void FacebookButtonAction() throws FacebookException {
-
-        Facebook facebook = new FacebookFactory().getInstance();
-        ResponseList<Post> feeds = facebook.getFeed("313850611958467",new Reading().limit(75));
-
-        Page pgId = facebook.getPage("313850611958467");
-        int likeCount = pgId.getLikes(); // aantal likes op de facebook pagina van rotterdam
-        for (int i = 0; i < feeds.size(); i++) {
-            Post post = feeds.get(i);
-            String message = post.getMessage();
-            Integer sharecount = post.getSharesCount();
-            String ID = post.getId();     // SQL MSG ID
-            if (sharecount == null) {
-                sharecount = 0;
-            }
-            // Print textarea test
-
-            try {
-                Connection con = connect.connectToDb();
-
-                String sql = "INSERT INTO Bericht (BerichtID,Datum, Beschrijving,socialmedia,Positief) VALUES (?,?,?,?,?)";
-                PreparedStatement preparedStatement = con.prepareStatement(sql);
-                preparedStatement.setString(1, ID);
-                preparedStatement.setDate(2, Date.valueOf(LocalDate.now()));
-                preparedStatement.setString(3, message);
-                preparedStatement.setString(4, "FaceBook");
-                preparedStatement.setInt(5, positief.getRating(message));
-                preparedStatement.execute();
-
-                sql = "INSERT INTO facebook (Bericht_BerichtID,likes,gedeeld) VALUES (?,?,?)";
-                PreparedStatement preparedStatement2 = con.prepareStatement(sql);
-                preparedStatement2.setString(1, ID);
-                preparedStatement2.setInt(2, likeCount);
-                preparedStatement2.setInt(3, sharecount);
-                preparedStatement2.execute();
-                System.out.println("Facebook data updated");
-
-
-                /**Close connection with Database **/
-                con.close();
-                /**Catch exception when data can't be saved into database for example: There is nothing filled in **/
-            } catch (SQLException e) {
-                System.out.println("geen nieuwe updates");
-                e.printStackTrace();
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-    @FXML// haalt facebook posts op van de facebook pagina Rotterdam centraal v2
-    private void FacebookData(){
-
-     try {
-        FacebookData fb = new FacebookData();
-     } catch (FacebookException e) {
-        e.printStackTrace();
-     }
-     try {
-        ResponseList<Post> fbmsges = new FacebookData().getFeeds();
-        for (int i = 0; i < fbmsges.size(); i++) {
-            Post post = fbmsges.get(i);
-            String message = post.getMessage();
-
-            System.out.println(message);
-        }
-     } catch (FacebookException e) {
-        e.printStackTrace();
-     }
-
-    }
     @FXML // Button voor het ophalen van het weer.
     private void WeerButtonAction() throws Exception {
         outputTempArea.setText("");
