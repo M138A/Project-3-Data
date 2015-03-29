a0f2649c683dc806822caaa4138605b57eedc0b6
barthr <bartfokker@gmail.com>
2015-03-29 23:36:48 +0200

    checkbox prototype nog niet helemaal af
    ga er morgen verder aan werken om het beter te maken, query's moeten we
    anders nog even over vergaderen

---
+++ checkbox prototype - bart/src/Data_Project/TestWindowController.java
@@ -0,0 +1,80 @@
+package Data_Project;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class TestWindowController implements Initializable {
+    public ToggleGroup raiting;
+    @FXML
+    private ToggleGroup socialmedia;
+    private String sql = "";
+
+
+    public void getSocialMediaGroup() {
+       try {
+           String toggle = ((RadioButton) socialmedia.selectedToggleProperty().getValue()).getText();
+           if(toggle.equals("Alles")){
+               sql = "";
+               sql += "SELECT COUNT(IF(socialmedia='Twitter',1,null)) 'twitter', COUNT(IF(socialmedia='Facebook',1,null)) 'facebook', COUNT(IF(socialmedia='Google',1,null)) 'google' FROM Bericht";
+               getPositiveOrNegative();
+           }
+           else {
+               sql = "";
+               sql += "SELECT COUNT(IF(socialmedia='" + toggle + "',1,null)) FROM Bericht";
+               getPositiveOrNegative();
+           }
+       }
+       catch (Exception e){
+           System.out.println("Klik een button aan om een analyse te doen!");
+       }
+    }
+
+    public String getPositiveOrNegative() {
+        String positief = "positief >= 5";
+        String negatief = "positief <= 5";
+        String allebei = "positief > -50";
+
+        try {
+            String toggle = ((RadioButton) raiting.selectedToggleProperty().getValue()).getText();
+            switch (toggle) {
+                case "Allebei":
+                    return sql += " WHERE" + " " + allebei;
+                case "Positief":
+                    return sql += " WHERE" + " " + positief;
+                default:
+                    return sql += " WHERE" + " " + negatief;
+            }
+        }catch (Exception e){
+            System.out.println("Kies!");
+        }
+        return null;
+    }
+
+    public void initialize(URL url, ResourceBundle rb) {
+        System.out.println("Test Window is open!");
+        System.out.println("\n\r");
+    }
+
+    public void testbutton(ActionEvent event) {
+        getSocialMediaGroup();
+        System.out.println(sql);
+        System.out.println();
+    }
+
+    /**
+     * SELECT Weersituatie, Bericht.Datum , Temperatuur, COUNT(socialmedia) FROM Bericht
+     LEFT OUTER JOIN Weersvoorspelling ON Weersvoorspelling.Datum = Bericht.Datum
+     WHERE positief > -50
+     GROUP BY DATUM;
+
+     */
+
+
+}
